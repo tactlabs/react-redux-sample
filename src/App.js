742f78ca49fc24678339a4ad008b8abdb3236cdc
Raja CSP Raman <raja.csp@gmail.com>
2019-09-03 21:11:25 -0400

    LoggedIn updated

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import {useSelector} from 'react-redux';
 function App() {
 
   const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state  => state.isLogged);
 
 
   return (
     <div className="App">
+      
       <h1>Counter  {counter}</h1>
 
+      
+
+      {isLogged ? 
+      <h3>Valuable Info (should be seen when only loggedin)</h3>
+      : 
+      ''
+      }
       
     </div>
   );
