f344fc54f22f93f9bd17d79a1bccea471592787a
Raja CSP Raman <raja.csp@gmail.com>
2019-09-03 21:14:50 -0400

    Increment action added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './App.css';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment} from './actions';
 
 function App() {
 
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state  => state.isLogged);
-
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       
       <h1>Counter  {counter}</h1>
 
-      
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
 
       {isLogged ? 
       <h3>Valuable Info (should be seen when only loggedin)</h3>
