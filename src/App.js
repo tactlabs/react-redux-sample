e399378096bc8616fba65f0a54c93ca0c6fa260a
Raja CSP Raman <raja.csp@gmail.com>
2019-09-03 21:22:17 -0400

    Add increment by 5 added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       
       <h1>Counter  {counter}</h1>
 
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
 
       {isLogged ? 
