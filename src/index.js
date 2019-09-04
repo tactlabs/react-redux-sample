02ef1234b35e913d51fb4877040e2e2d1cd73d1e
Raja CSP Raman <raja.csp@gmail.com>
2019-09-03 20:23:13 -0400

    Base redux code added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'; // create globalized state
+
+
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION - INCREMENT
+const increment = () => {
+    return {
+        type : 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type : 'DECREMENT'
+    }
+}
+
+// REDUCER 
+const counter = (state = 0, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        
+        case 'DECREMENT':
+            return state - 1;
+    }
+}
+
+let store = createStore(counter);
+
+// display it in the console
+store.subscribe( () => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
